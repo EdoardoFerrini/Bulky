427db9f3265b4e184d91b0be63aea453ac9092f1
EdoardoFerrini <edoardo.ferrini@gmail.com>
2024-02-23 16:52:11 +0100

    Aggiunto Category Controller + View + import

---
+++ Bulky.Web/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using Bulky.DataAccess.Data;
+using Bulky.Models.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bulky.Web.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            var categoryList = _db.Categories.ToList();
+            return View(categoryList);
+        }
+    }
+}
