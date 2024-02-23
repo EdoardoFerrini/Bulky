47c9cc80912ff13b7c652ab5fe17786252e41a01
EdoardoFerrini <edoardo.ferrini@gmail.com>
2024-02-23 14:47:24 +0100

    Aggiungere i file di progetto.

---
+++ Bulky.Domain/Models/Category.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulky.Models.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(30)]
+        [DisplayName("Category Name")]
+        public string Name { get; set; }
+        [DisplayName("Category Name")]
+        [Range(1,100, ErrorMessage ="Display Order must been between 1-100")]
+        public int DisplayOrder { get; set; }
+    }
+}
