1a4bb19d25216912b75da38b7e4adb2db6bb9e08
EdoardoFerrini <edoardo.ferrini@gmail.com>
2024-03-01 11:04:18 +0100

    Aggiunto ShoppingCart controller con metodi per ricavare il prezzo e aumentare/diminuire la quantità dei prodotti sul carrello

---
+++ Bulky.Web/Areas/Customer/Controllers/ShoppingCartController.cs
@@ -0,0 +1,89 @@
+﻿using Bulky.DataAccess.Data;
+using Bulky.DataAccess.Repository;
+using Bulky.DataAccess.Repository.IRepository;
+using Bulky.Models.Models;
+using Bulky.Models.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace Bulky.Web.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    [Authorize]
+    public class ShoppingCartController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public ShoppingCartVm shoppingCartVm { get; set; }
+        public ShoppingCartController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+            
+            ShoppingCartVm shoppingCartVm = new ShoppingCartVm()
+            {
+                ShoppingCartList = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == userId, includeProperties: "Product"),
+            };
+
+            foreach(var cart in shoppingCartVm.ShoppingCartList)
+            {
+                cart.Price = GetPriceBasedOnQuantity(cart);
+                shoppingCartVm.OrderTotal += (cart.Price * cart.Count);
+            }
+
+            return View(shoppingCartVm);
+        }
+
+        private double GetPriceBasedOnQuantity(ShoppingCart shoppingCart) 
+        { 
+            if (shoppingCart.Count <= 50) 
+            {
+                return shoppingCart.Product.Price;
+            }
+            else if(shoppingCart.Count <= 100)
+            {
+                return shoppingCart.Product.Price50;
+            }
+            else
+            {
+                return shoppingCart.Product.Price100;
+            }
+        }
+        public IActionResult Plus(int cartId)
+        {
+            var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId);
+            cartFromDb.Count += 1;
+            _unitOfWork.ShoppingCart.Update(cartFromDb);
+            _unitOfWork.Save();
+
+            return RedirectToAction(nameof(Index));
+        }
+        public IActionResult Minus(int cartId)
+        {
+            var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId);
+            if(cartFromDb.Count <= 1)
+            {
+                _unitOfWork.ShoppingCart.Remove(cartFromDb);
+            }
+            else
+            {
+                cartFromDb.Count -= 1;
+                _unitOfWork.ShoppingCart.Update(cartFromDb);
+            }
+
+            _unitOfWork.Save();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Remove(int cartId) 
+        {
+            var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
