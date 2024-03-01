26aa025be2838f7d7725b702fa54cf07d06d95bd
EdoardoFerrini <edoardo.ferrini@gmail.com>
2024-03-01 09:02:58 +0100

    Aggiunto il no tracking

--- Bulky.Application/Repository/IRepository/IRepository.cs
+++ Bulky.Application/Repository/IRepository/IRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
@@ -10,7 +11,7 @@ namespace Bulky.DataAccess.Repository.IRepository
     public interface IRepository<T> where T : class
     {
         IEnumerable<T> GetAll(Expression<Func<T, bool>>? filter = null, string? includeProperties = null);
-        T Get(Expression<Func<T, bool>> filter, string? includeProperties = null);
+        T Get(Expression<Func<T, bool>> filter, string? includeProperties = null, bool tracked = false);
         void Add(T entity);
         void Remove(T entity);
 
